8dbb7b330a2b61dab509a2045a9cebc4c661c1f7
Pavel <pavel.shalyga.1992@gmail.com>
2024-06-10 04:26:55 +0300

    Create integration test for checkout process

---
+++ spec/integrations/checkout_process_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'money'
+require_relative '../../lib/services/checkout_service'
+require_relative '../../lib/models/product'
+require_relative '../../lib/models/basket'
+require_relative '../../lib/models/pricing_rules/absolute_pricing_rule'
+require_relative '../../lib/models/pricing_rules/percentage_pricing_rule'
+require_relative '../../lib/models/pricing_rules/bonus_product_pricing_rule'
+require_relative '../../lib/repositories/pricing_rules/pricing_rule_hash_repository'
+require_relative '../../lib/repositories/products/product_hash_repository'
+require_relative '../../lib/errors/validation_error'
+
+RSpec.describe 'Checkout process' do
+  let(:pricing_rule_repository) { PricingRuleHashRepository.new }
+  let(:product_repository) { ProductHashRepository.new }
+  let(:checkout_service) { CheckoutService.new(pricing_rule_repository, product_repository) }
+
+  before do
+    pricing_rule_repository.create(AbsolutePricingRule.new('SR1', 3, Money.from_cents(450, 'GBP')))
+    pricing_rule_repository.create(PercentagePricingRule.new('CF1', 3, (1 - Rational(2, 3)) * 100))
+    pricing_rule_repository.create(BonusProductPricingRule.new('GR1', 1))
+
+    product_repository.create(Product.new('GR1', 'Green tea', 311, 'GBP'))
+    product_repository.create(Product.new('SR1', 'Strawberries', 500, 'GBP'))
+    product_repository.create(Product.new('CF1', 'Coffee', 1123, 'GBP'))
+  end
+
+  it 'calculates the total price correctly for a basket with GR1, SR1, GR1, GR1, CF1 products' do
+    basket = Basket.new
+    basket.add_item('GR1')
+    basket.add_item('SR1')
+    basket.add_item('GR1')
+    basket.add_item('GR1')
+    basket.add_item('CF1')
+    expect(checkout_service.checkout(basket)).to eq(Money.from_cents(2245, 'GBP'))
+  end
+
+  it 'calculates the total price correctly for a basket with GR1, GR1 products' do
+    basket = Basket.new
+    basket.add_item('GR1')
+    basket.add_item('GR1')
+    expect(checkout_service.checkout(basket)).to eq(Money.from_cents(311, 'GBP'))
+  end
+
+  it 'calculates the total price correctly for a basket with SR1, SR1, GR1, SR1 products' do
+    basket = Basket.new
+    basket.add_item('SR1')
+    basket.add_item('SR1')
+    basket.add_item('GR1')
+    basket.add_item('SR1')
+    expect(checkout_service.checkout(basket)).to eq(Money.from_cents(1661, 'GBP'))
+  end
+
+  it 'calculates the total price correctly for a basket with GR1, CF1, SR1, CF1, CF1 products' do
+    basket = Basket.new
+    basket.add_item('GR1')
+    basket.add_item('CF1')
+    basket.add_item('SR1')
+    basket.add_item('CF1')
+    basket.add_item('CF1')
+    expect(checkout_service.checkout(basket)).to eq(Money.from_cents(3057, 'GBP'))
+  end
+
+  it 'raises an error if the basket is empty' do
+    basket = Basket.new
+    expect do
+      checkout_service.checkout(basket)
+    end.to raise_error(ValidationError,
+                       'Validation failed: You must scan products to get total price')
+  end
+end
